a204c0f5dcffbf2bf1894810ad11668ddd083a65
Marcel Fest <marcel.fest@telekom.de>
2026-02-25 07:50:52 +0100

    Use SendmsgBuffers from socket
    Signed-off-by: Marcel Fest <marcel.fest@telekom.de>

--- conn_linux.go
+++ conn_linux.go
@@ -92,18 +92,18 @@ func newConn(s *socket.Conn, config *Config) (*conn, uint32, error) {
 
 // SendMessages serializes multiple Messages and sends them to netlink.
 func (c *conn) SendMessages(messages []Message) error {
-	var buf []byte
-	for _, m := range messages {
+	buffers := make([][]byte, len(messages))
+	for i, m := range messages {
 		b, err := m.MarshalBinary()
 		if err != nil {
 			return err
 		}
 
-		buf = append(buf, b...)
+		buffers[i] = b
 	}
 
 	sa := &unix.SockaddrNetlink{Family: unix.AF_NETLINK}
-	_, err := c.s.Sendmsg(context.Background(), buf, nil, sa, 0)
+	_, err := c.s.SendmsgBuffers(context.Background(), buffers, nil, sa, 0)
 	return err
 }
 
